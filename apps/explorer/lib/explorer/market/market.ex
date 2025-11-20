750ab4bfb8e7fc27333f4a1985e02eeb5d499ebe
Maxim Filonov <53992153+sl1depengwyn@users.noreply.github.com>
2025-11-20 17:31:27 +0300

    Employ distributed elixir runtime
    [no ci]

--- apps/explorer/lib/explorer/market/market.ex
+++ apps/explorer/lib/explorer/market/market.ex
@@ -3,8 +3,51 @@ defmodule Explorer.Market do
   Context for data related to the cryptocurrency market.
   """
 
-  alias Explorer.Market.Fetcher.{Coin, History}
+  use GenServer
+
+  alias Explorer.Helper
+  alias Explorer.Market.Fetcher.{Coin, History, Token}
   alias Explorer.Market.{MarketHistory, MarketHistoryCache, Token}
+
+  @history_key :market_history_fetcher_enabled
+  @tokens_key :market_token_fetcher_enabled
+
+  def init(_opts) do
+    if Explorer.mode() == :all do
+      {history_pid, token_pid} = find_history_and_token_fetchers()
+      :persistent_term.put(@history_key, !is_nil(history_pid))
+      :persistent_term.put(@tokens_key, !is_nil(token_pid))
+      :ignore
+    else
+      {:ok, nil, {:continue, 1}}
+    end
+  end
+
+  def handle_continue(attempt, _state) do
+    attempt |> :timer.seconds() |> :timer.sleep()
+
+    case Node.list()
+         |> Enum.filter(&Helper.indexer_node?/1) do
+      [] ->
+        if attempt < 5 do
+          {:noreply, nil, {:continue, attempt * 2}}
+        else
+          raise "No indexer nodes discovered after #{attempt} attempts"
+        end
+
+      [indexer] ->
+        {history_pid, token_pid} =
+          :rpc.call(indexer, __MODULE__, :find_history_and_token_fetchers, [])
+          |> Helper.process_rpc_response(indexer, {nil, nil})
+
+        :persistent_term.put(@history_key, !is_nil(history_pid))
+        :persistent_term.put(@tokens_key, !is_nil(token_pid))
+        {:stop, :normal}
+
+      multiple_indexers ->
+        raise "Multiple indexer nodes discovered: #{inspect(multiple_indexers)}"
+    end
+  end
 
   @doc """
   Retrieves the history for the recent specified amount of days.
@@ -13,7 +56,7 @@ defmodule Explorer.Market do
   """
   @spec fetch_recent_history(boolean()) :: [MarketHistory.t()]
   def fetch_recent_history(secondary_coin? \\ false) do
-    if GenServer.whereis(History) do
+    if :persistent_term.get(@history_key, nil) do
       MarketHistoryCache.fetch(secondary_coin?)
     else
       []
@@ -63,4 +106,10 @@ defmodule Explorer.Market do
     |> MarketHistory.price_at_date(false, options)
     |> MarketHistory.to_token()
   end
+
+  def token_fetcher_enabled? do
+    :persistent_term.get(@tokens_key, false)
+  end
+
+  def find_history_and_token_fetchers, do: {GenServer.whereis(History), GenServer.whereis(Token)}
 end
